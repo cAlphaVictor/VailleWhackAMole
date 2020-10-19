b9f69574ff7d39e839bd3fbd5e0e1e541fb88ad9
Ciaran Vaille <ciaran.vaille.154@k12.friscoisd.org>
2020-10-19 08:54:40 -0500

    Initial Commit

---
+++ VailleWhackAMole/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VailleWhackAMole
+//
+//  Created by Vaille, Ciaran A on 10/19/20.
+//  Copyright © 2020 Vaille, Ciaran A. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
